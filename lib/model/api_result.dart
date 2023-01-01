f98cb00d3c0388fbb813cdecc422c8ef54487aee
Kathiravan Sekar <kathiravanrs@gmail.com>
2023-01-01 16:32:04 -0500

    Added Image Picker and cropper. Custom class for API response. Show group names on screen

---
+++ lib/model/api_result.dart
@@ -0,0 +1,124 @@
+class ApiResult {
+  ApiResult({
+    required this.isFood,
+    required this.results,
+  });
+  late final bool isFood;
+  late final List<Results> results;
+
+  ApiResult.fromJson(Map<String, dynamic> json) {
+    isFood = json['is_food'];
+    results = List.from(json['results']).map((e) => Results.fromJson(e)).toList();
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['is_food'] = isFood;
+    data['results'] = results.map((e) => e.toJson()).toList();
+    return data;
+  }
+}
+
+class Results {
+  Results({
+    required this.items,
+    required this.group,
+  });
+  late final List<Items> items;
+  late final String group;
+
+  Results.fromJson(Map<String, dynamic> json) {
+    items = List.from(json['items']).map((e) => Items.fromJson(e)).toList();
+    group = json['group'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['items'] = items.map((e) => e.toJson()).toList();
+    data['group'] = group;
+    return data;
+  }
+}
+
+class Items {
+  Items({
+    required this.servingSizes,
+    required this.score,
+    required this.nutrition,
+    required this.name,
+    required this.foodId,
+    required this.group,
+  });
+  late final List<ServingSizes> servingSizes;
+  late final int score;
+  late final Nutrition nutrition;
+  late final String name;
+  late final String foodId;
+  late final String? group;
+
+  Items.fromJson(Map<String, dynamic> json) {
+    servingSizes = List.from(json['servingSizes']).map((e) => ServingSizes.fromJson(e)).toList();
+    score = json['score'];
+    nutrition = Nutrition.fromJson(json['nutrition']);
+    name = json['name'];
+    foodId = json['food_id'];
+    group = json['group'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['servingSizes'] = servingSizes.map((e) => e.toJson()).toList();
+    data['score'] = score;
+    data['nutrition'] = nutrition.toJson();
+    data['name'] = name;
+    data['food_id'] = foodId;
+    data['group'] = group;
+    return data;
+  }
+}
+
+class ServingSizes {
+  ServingSizes({
+    required this.unit,
+  });
+  late final String? unit;
+
+  ServingSizes.fromJson(Map<String, dynamic> json) {
+    unit = json['unit'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['unit'] = unit;
+    return data;
+  }
+}
+
+class Nutrition {
+  Nutrition({
+    required this.totalCarbs,
+    required this.totalFat,
+    required this.protein,
+    required this.calories,
+  });
+  late final double totalCarbs;
+  late final double totalFat;
+  late final double protein;
+  late final double calories;
+
+  Nutrition.fromJson(Map<String, dynamic> json) {
+    totalCarbs = json['totalCarbs'];
+    totalFat = json['totalFat'];
+    protein = json['protein'];
+    calories = json['calories'] * 1.0;
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['totalCarbs'] = totalCarbs;
+    data['totalFat'] = totalFat;
+    data['protein'] = protein;
+    data['calories'] = calories;
+    return data;
+  }
+}
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -3,6 +3,8 @@ import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_native_image/flutter_native_image.dart';
+import 'package:foodradar/model/api_result.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 
@@ -18,30 +20,37 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String disp = "Image not uploaded yet";
+  var appBar = AppBar(
+    elevation: 0,
+    centerTitle: true,
+    title: const Text("Food Radar"),
+  );
+
+  var bottomBar = const BottomAppBar(
+    shape: CircularNotchedRectangle(),
+    notchMargin: 5,
+    child: Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Text(""),
+    ),
+  );
+
+  String displayText = "Image not uploaded yet";
+  bool imageSelected = false;
+
+  late File img;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: const BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        notchMargin: 5,
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Text(""),
-        ),
-      ),
+      appBar: appBar,
+      bottomNavigationBar: bottomBar,
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       extendBody: true,
       floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.abc),
-        onPressed: () {
-          uploadFile();
-        },
-      ),
-      appBar: AppBar(
-        elevation: 0,
-        centerTitle: true,
-        title: const Text("Food Radar"),
+        backgroundColor: Colors.white,
+        onPressed: getImage,
+        child: const Icon(Icons.camera_alt_outlined),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -49,7 +58,10 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(disp),
+              if (imageSelected) Image.file(img),
+              TextButton(onPressed: crop, child: Text("Crop")),
+              TextButton(onPressed: uploadFile, child: Text("upload")),
+              Text(displayText),
             ],
           ),
         ),
@@ -61,31 +73,48 @@ class _HomePageState extends State<HomePage> {
     var request = http.MultipartRequest(
         'POST',
         Uri.parse(
-            'https://api-2445582032290.production.gw.apicast.io/v1/foodrecognition?user_key=$api_key'));
+          'https://api-2445582032290.production.gw.apicast.io/v1/foodrecognition?user_key=$api_key',
+        ));
 
-    File file = await getImage();
+    File file = img;
     request.files.add(http.MultipartFile.fromBytes('picture', File(file.path).readAsBytesSync(),
         filename: file.path));
 
     var response = await request.send();
     var decodedResponse = await http.Response.fromStream(response);
     final responseData = json.decode(decodedResponse.body);
-
-    if (response.statusCode == 200) {
-      print("SUCCESS");
-    } else {
-      print("ERROR");
-    }
+    var result = ApiResult.fromJson(responseData);
     setState(() {
-      disp = responseData.toString();
+      for (Results r in result.results) {
+        displayText = "$displayText ${r.group}";
+      }
+      // displayText = result.results.toString();
     });
+
     log(responseData.toString());
   }
 
   Future<File> getImage() async {
     final ImagePicker picker = ImagePicker();
-    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+    final XFile? image =
+        await picker.pickImage(maxHeight: 544, maxWidth: 544, source: ImageSource.gallery);
     File file = File(image!.path);
+    setState(() {
+      img = file;
+      imageSelected = true;
+    });
     return file;
+  }
+
+  crop() async {
+    File compressedFile = await FlutterNativeImage.compressImage(
+      img.path,
+      quality: 80,
+      targetWidth: 544,
+      targetHeight: 544,
+    );
+    setState(() {
+      img = compressedFile;
+    });
   }
 }
